de7764e30aa9229c5fee7c542fd07b18f6a5b216
Samuel20354 <68336568+Samuel20354@users.noreply.github.com>
2021-01-25 20:18:05 +0000

    Update SkiptracerUK.py

--- SkiptracerUK.py
+++ SkiptracerUK.py
@@ -16,9 +16,9 @@ print("""
 ░╚═══██╗██╔═██╗░██║██╔═══╝░░░░██║░░░██╔══██╗██╔══██║██║░░██╗██╔══╝░░██╔══██╗
 ██████╔╝██║░╚██╗██║██║░░░░░░░░██║░░░██║░░██║██║░░██║╚█████╔╝███████╗██║░░██║
 ╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚══════╝╚═╝░░╚═╝
-                                                       Coded By Samuel20354
+                                                        Coded By Samuel20354
 
-Please type the UK car plate in LOWERCASE (XXXXXX)
+Please type the UK car plate in LOWERCASE (xxxxxxx)
 
 """)
 
@@ -99,7 +99,7 @@ for p in p_lst:
         vehicle_age = match.group(1)
 
 for p in p_lst:
-    match = re.match("Registered Date: (.*)",p.text)
+    match = re.match("Year: (.*)",p.text)
 
     if match:
         registered_date = match.group(1)
@@ -114,7 +114,7 @@ print("""
 ░╚═══██╗██╔═██╗░██║██╔═══╝░░░░██║░░░██╔══██╗██╔══██║██║░░██╗██╔══╝░░██╔══██╗
 ██████╔╝██║░╚██╗██║██║░░░░░░░░██║░░░██║░░██║██║░░██║╚█████╔╝███████╗██║░░██║
 ╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚══════╝╚═╝░░╚═╝
-                                                       Coded By Samuel20354
+                                                        Coded By Samuel20354
 
 """)      
 print("")
@@ -130,7 +130,7 @@ print("Top Speed: "+(top_speed))
 print("BHP: "+(bhp))
 print("AVG Yearly Mileage: "+(yearly_mileage))
 print("Vehicle Age: "+(vehicle_age))
-print("Registered Date: "+(registered_date))
+print("Year: "+(registered_date))
 
 print("")
 print("")
@@ -188,7 +188,7 @@ for p in d_lst:
     total_mileage_rec = match.group(1)
 
 for p in p_lst:
-    match = re.match("Last Mileage Record: (.*)",p.text)
+    match = re.match("Last Record: (.*)",p.text)
 
     if match:
         last_mileage_rec = match.group(1)
@@ -210,33 +210,5 @@ print("Last Mileage Record: "+(last_mileage_rec))
 print("AVG Yearly Mileage: "+(avg_yearly_mileage))
 print("Estimated Total Mileage Now: "+(est_total_mileage))
 
-print("")
-print("")
-print(Fore.RED + "Latest MOT Records:" + Fore.RESET)
-print("")
-mot_lst = []
-cur_dict = {}
-for div in d_lst:
-    date_match = re.match("TEST DATE(.*)",div.text.strip())
-    if date_match:
-        cur_dict["test date"] = date_match.group(1)
-
-    result_match = re.match("RESULT(.*)",div.text.strip())
-    if result_match:
-        cur_dict["result"] = result_match.group(1)
-
-    mileage_match = re.match("MILEAGE(.*)",div.text.strip())
-    if mileage_match:
-        if "test date" in cur_dict and "result" in cur_dict:
-            cur_dict["mileage"] = mileage_match.group(1)
-            mot_lst.append(cur_dict)
-            cur_dict = {}
-
-print(f'{"TEST DATE":^11} | {"RESULT":^6} | {"MILEAGE":^10} ')
-for mot in mot_lst:
-    print(f'{mot["test date"]:^11} | {mot["result"]:^6} | {mot["mileage"]:^10}')
-
-
-print("")
 print("")
 print(Fore.GREEN + "Done!" + Fore.RESET)
