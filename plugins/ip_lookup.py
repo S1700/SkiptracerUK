e10afb34c8f18aac091f688b1c3e6e6837eb9ae1
Samuel20354 <68336568+Samuel20354@users.noreply.github.com>
2021-06-04 19:59:30 +0100

    Update ip_lookup.py

--- plugins/ip_lookup.py
+++ plugins/ip_lookup.py
@@ -34,7 +34,7 @@ def lookup():
                  "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
 
     for i in range(len(animation)):
-        time.sleep(0.3)
+        time.sleep(0.2)
         sys.stdout.write("\r" + animation[i % len(animation)])
         sys.stdout.flush()
 
