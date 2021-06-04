9c41580d596267cf4ecd820461321ab5f5576ce1
Samuel20354 <68336568+Samuel20354@users.noreply.github.com>
2021-06-04 17:40:07 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+# Description:
+ 
+Like Skiptracer but for the uk.
+SkiptracerUK peforms recon attacks and finds information about a target. SkiptracerUK mainly uses basic python webscraping.
+
+
+#What can it do:
+Skiptracer will alow you to search with the following queries:
+
+* UK car plate search
+* Usernames
+* IP addresses
+
+More search queries coming soon!
+
+# How to install and use SkiptracerUK:
+
+* Download the repository with `git clone https://github.com/Samuel20354/SkiptracerUK.git`
+* run `cd SkiptracerUK`
+* then run `python3 -m pip install -r requirements.txt`
+* and finaly run `python3 SkiptracerUK.py`
+
+Done, enjoy SkiptracerUK!
+
+#Bugs that I know of:
+
+* `FileNotFoundError: [Errno 2] No such file or directory: 'plugins/'` this bug is temporary just run SkiptracerUK again, and it should be fixed. Idk why it happens.
+* When exiting using the method in the script it some times doesn't exit just use the same method again and it will exit. 
+* Some times when running plate or ip search it wont return you back to the menu just run the script again.
+
+#### Credit to Critical-Function956 over at reddit for helping me out
---
+++ SkiptracerUK.py
@@ -0,0 +1,64 @@
+import os
+import time
+from colorama import Fore
+from plugins.plate_search import plate
+from plugins.ip_lookup import lookup
+
+
+def main_menu():
+    os.system("clear")
+
+    print("""
+    
+    \033[1;32m░██████╗██╗░░██╗██╗██████╗░████████╗██████╗░░█████╗░░█████╗░███████╗██████╗░
+    ██╔════╝██║░██╔╝██║██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+    ╚█████╗░█████═╝░██║██████╔╝░░░██║░░░██████╔╝███████║██║░░╚═╝█████╗░░██████╔╝
+    ░╚═══██╗██╔═██╗░██║██╔═══╝░░░░██║░░░██╔══██╗██╔══██║██║░░██╗██╔══╝░░██╔══██╗
+    ██████╔╝██║░╚██╗██║██║░░░░░░░░██║░░░██║░░██║██║░░██║╚█████╔╝███████╗██║░░██║
+    ╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚══════╝╚═╝░░╚═╝
+                                                            Coded By Samuel20354
+    
+    
+1. Plate search
+2. Sherlock
+3. IP search
+4. Exit
+ -""")
+
+    option = str(input("OPTION: "))
+
+    if option == "1":
+        plate()
+
+    elif option == "2":
+        print(" -")
+        sherlock_username = input("Username to scan for: ")
+        print("")
+        os.system("clear")
+        plugins = "plugins/"
+        os.chdir(plugins)
+        os.system("python3 sherlock.py " + sherlock_username)
+        input(Fore.BLUE + "Press Enter to continue..." + Fore.RESET)
+        main_menu()
+
+
+    elif option == "3":
+        lookup()
+
+    elif option == "4":
+        print(" -")
+        print("Goodbye! [\033[0m\]")
+        exit()
+
+    else:
+        print(" -")
+        print("Invalid option, please try again.")
+        print(" -")
+        time.sleep(5)
+        main_menu()
+
+
+if __name__ == "__main__":
+    main_menu()
+
+main_menu()
